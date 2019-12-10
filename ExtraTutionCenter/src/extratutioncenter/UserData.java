f8a0b84766709cb385f32a2207a70176f3dd180f
shiva <shiva@LAPTOP-D47K26VG>
2019-12-10 11:04:41 +0000

    getter and setter methods

--- ExtraTutionCenter/src/extratutioncenter/UserData.java
+++ ExtraTutionCenter/src/extratutioncenter/UserData.java
@@ -24,6 +24,46 @@ public class UserData {
     }
        
     private String name,gender,address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public long getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(long contactNo) {
+        this.contactNo = contactNo;
+    }
     private String DOB;
     private long contactNo;
     
